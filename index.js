c26cef742c0c9ea413866bad00fcedf8166fa783
niteshkumar45 <138018465+niteshkumar45@users.noreply.github.com>
2023-09-28 10:40:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get("/",(req,res)=>{
-    log("route access");
+    console.log("route access");
     res.send("this is home page")
 })
 
